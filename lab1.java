222b1ffbd48c75fa8b71401d82643936e61f23e5
Adrian Kirchner <akirchner@hawk.iit.edu>
2020-02-12 17:28:02 -0600

    renamed lab1 class

--- lab1.java
+++ lab1.java
@@ -1,4 +1,4 @@
-class Lab1 {
+class lab1 {
     public static int[] unsorted;
     public static int[] mergeResult;
     public static int[] insertionResult;
@@ -62,7 +62,9 @@ class Lab1 {
 
     static void insertionSort(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            
+            for (int j = i; j > 0; j--) {
+                if ()
+            }
         }
     }
 }
