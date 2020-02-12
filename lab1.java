74eb50d007fa889a3dfe7e36d5e6305ef561d156
Adrian Kirchner <akirchner@hawk.iit.edu>
2020-02-12 17:14:19 -0600

    finished merge

--- lab1.java
+++ lab1.java
@@ -24,6 +24,31 @@ class Lab1 {
         return null;
     }
 
+    void merge (int[] array, int start, int split, int end) { //start index, index beginning the second array, and index after the end of the second array
+        int[] a = new int[split - start];
+        int[] b = new int[end - split];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = array[i + start];
+        }
+        for (int i = 0; i < b.length; i++) {
+            b[i] = array[i + split];
+        }
+
+        int ai = 0;
+        int bi = 0;
+        for (int i = start; i < end; i++) {
+            if (ai == a.length || a[ai] > b[bi]) {
+                array[i] = b[bi];
+                bi++;
+            }
+            else if (bi == b.length || a[ai] <= b[bi]) {
+                array[i] = a[ai];
+                ai++;
+            }
+        }
+    }
+
     void mergeSort() {
 
     }
