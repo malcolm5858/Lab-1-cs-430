9fa163b36f99a9ff6a387ab5d3d3afa0d22d9b6c
Adrian Kirchner <akirchner@hawk.iit.edu>
2020-02-13 01:32:22 -0600

    separated out sort functions to their own classes

---
+++ MergeSort.java
@@ -0,0 +1,35 @@
+public class MergeSort {
+    static void merge(int[] array, int start, int split, int end) { // start index, index beginning the second array,
+                                                                           // and index after the end of the second array
+        int[] a = new int[split - start]; //new arrays for each half of the merge
+        int[] b = new int[end - split];
+
+        for (int i = 0; i < a.length; i++) { //copying from main array into new arrays a and b
+            a[i] = array[i + start];
+        }
+        for (int i = 0; i < b.length; i++) {
+            b[i] = array[i + split];
+        }
+
+        int ai = 0; //increment variables for each of the temporary arrays
+        int bi = 0;
+        for (int i = start; i < end; i++) { //increment through the relevant section of the array
+            if (ai == a.length || a[ai] > b[bi]) { //if the next lowest element is in b, it gets transferred.
+                array[i] = b[bi];                  //we also check if we're done iterating through a here.
+                bi++;
+            } else if (bi == b.length || a[ai] <= b[bi]) { //otherwise, the element in a gets transferred
+                array[i] = a[ai];
+                ai++;
+            }
+        }
+    }
+
+    public static void sort(int[] array, int start, int end) { //we take the array, and a start and end index for recursion
+        if (start < end - 1) { //empty base case because the entire sorting operation takes place in the merge
+            int split = (start + end) / 2; //integer division gives the floor, so this gives an adequate split location
+            sort(array, start, split); //recursion on each half of the array
+            sort(array, split, end);
+            merge(array, start, split, end); //merge operation on each sorted half
+        }
+    }
+}
--- lab1.java
+++ lab1.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Random;
 
 public class lab1 {
@@ -13,19 +12,19 @@ public class lab1 {
         insertionResult = unsorted; //copy into new arrays for each function
         mergeResult = unsorted;
 
-        System.out.println(Arrays.toString(unsorted)); // prints unsorted array to console so you can confirm results
+        //System.out.println(Arrays.toString(unsorted)); // prints unsorted array to console so you can confirm results
 
         long start = System.currentTimeMillis(); //gets current system time for performance calculation
-        insertionSort(insertionResult); //perform insertion sort
+        InsertionSort.sort(insertionResult); //perform insertion sort
         insertionTime = System.currentTimeMillis() - start; //subtract current system time from start value to get performance results
 
-        System.out.println(Arrays.toString(insertionResult)); //prints sorted array to console for confirmation
+        //System.out.println(Arrays.toString(insertionResult)); //prints sorted array to console for confirmation
 
         start = System.currentTimeMillis(); //gets current system time for performance calculation
-        mergeSort(mergeResult, 0, arraySize); //perform merge sort
+        MergeSort.sort(mergeResult, 0, arraySize); //perform merge sort
         mergeTime = System.currentTimeMillis() - start; //subtract current system time from start value to get performance results
 
-        System.out.println(Arrays.toString(mergeResult)); //prints sorted array to console for confirmation
+        //System.out.println(Arrays.toString(mergeResult)); //prints sorted array to console for confirmation
 
         return new long[] { insertionTime, mergeTime }; //returns performance results to frontend
     }
@@ -39,53 +38,5 @@ public class lab1 {
         }
 
         return array;
-    }
-
-    static void merge(int[] array, int start, int split, int end) { // start index, index beginning the second array,
-                                                                    // and index after the end of the second array
-        int[] a = new int[split - start]; //new arrays for each half of the merge
-        int[] b = new int[end - split];
-
-        for (int i = 0; i < a.length; i++) { //copying from main array into new arrays a and b
-            a[i] = array[i + start];
-        }
-        for (int i = 0; i < b.length; i++) {
-            b[i] = array[i + split];
-        }
-
-        int ai = 0; //increment variables for each of the temporary arrays
-        int bi = 0;
-        for (int i = start; i < end; i++) { //increment through the relevant section of the array
-            if (ai == a.length || a[ai] > b[bi]) { //if the next lowest element is in b, it gets transferred.
-                array[i] = b[bi];                  //we also check if we're done iterating through a here.
-                bi++;
-            } else if (bi == b.length || a[ai] <= b[bi]) { //otherwise, the element in a gets transferred
-                array[i] = a[ai];
-                ai++;
-            }
-        }
-    }
-
-    static void mergeSort(int[] array, int start, int end) { //we take the array, and a start and end index for recursion
-        if (start < end - 1) { //empty base case because the entire sorting operation takes place in the merge
-            int split = (start + end) / 2; //integer division gives the floor, so this gives an adequate split location
-            mergeSort(array, start, split); //recursion on each half of the array
-            mergeSort(array, split, end);
-            merge(array, start, split, end); //merge operation on each sorted half
-        }
-    }
-
-    static void insertionSort(int[] array) { //just takes an array, not recursive
-        for (int i = 0; i < array.length; i++) { //iterate through array
-            for (int j = i; j > 0; j--) { //iterate backwards to find insertion point
-                if (array[j] < array[j - 1]) { //check against previous element to see it should go further
-                    int temp = array[j]; //swap array[j] and array[j-1]
-                    array[j] = array[j - 1];
-                    array[j - 1] = temp;
-                } else { //if we don't need to go back further, we're done inserting this element and can exit the inner loop
-                    break;
-                }
-            }
-        }
     }
 }
